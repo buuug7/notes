60ea09a267e5a1b005bcfbd55ca27d9cf91ca19e
buuug7 <youpp@126.com>
2021-04-10 19:10:02 +0800

    update

--- javascript/algorithms/quick-sort/quick-sort.test.ts
+++
@@ -1,7 +0,0 @@
-import { quickSort } from "./quick-sort";
-
-it("should sort number array", function () {
-  const arr = [3, 2, 1, 4, 5];
-  expect(quickSort(arr)[0]).toBe(1);
-  expect(quickSort(arr)[4]).toBe(5);
-});
